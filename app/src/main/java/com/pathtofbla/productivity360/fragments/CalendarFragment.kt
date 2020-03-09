5c5cf5c184cfa8c6b34182651c7e201f1561b033
Ninad Kulkarni <ninadk03@gmail.com>
2020-03-08 19:48:48 -0700

    Finished implementing calendarview library. Calendar skeleton works. Need to add data-driven functionalities.
    Signed-off-by: Ninad Kulkarni <ninadk03@gmail.com>

--- app/src/main/java/com/pathtofbla/productivity360/fragments/CalendarFragment.kt
+++ app/src/main/java/com/pathtofbla/productivity360/fragments/CalendarFragment.kt
@@ -1,22 +1,37 @@
 package com.pathtofbla.productivity360.fragments
 
+import android.graphics.Color
 import android.os.Bundle
-import android.util.Log
 import android.view.*
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.jakewharton.threetenabp.AndroidThreeTen
 import com.kizitonwose.calendarview.model.CalendarDay
+import com.kizitonwose.calendarview.model.DayOwner
 import com.kizitonwose.calendarview.ui.DayBinder
 import com.kizitonwose.calendarview.ui.ViewContainer
-import com.pathtofbla.productivity360.FormatDate
 import com.pathtofbla.productivity360.R
+import kotlinx.android.synthetic.main.app_bar_main.*
 import kotlinx.android.synthetic.main.calendar_day_layout.view.*
+import kotlinx.android.synthetic.main.calendar_header_layout.view.*
 import kotlinx.android.synthetic.main.fragment_calendar.*
+import org.threeten.bp.LocalDate
 import org.threeten.bp.YearMonth
+import org.threeten.bp.format.DateTimeFormatter
 import org.threeten.bp.temporal.WeekFields
 import java.util.*
 
 class CalendarFragment : Fragment() {
+
+    private var selectedDate: LocalDate? = null
+    private lateinit var today: LocalDate
+
+    private val titleSameYearFormatter = DateTimeFormatter.ofPattern("MMMM")
+    private val titleFormatter = DateTimeFormatter.ofPattern("MMM yyyy")
+    private val selectionFormatter = DateTimeFormatter.ofPattern("d MMM yyyy")
+//    private val events = mutableMapOf<LocalDate, List<Event>>()
+
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -30,6 +45,7 @@ class CalendarFragment : Fragment() {
 
         setHasOptionsMenu(true)
         AndroidThreeTen.init(this.context)
+        today = LocalDate.now()
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
@@ -48,15 +64,6 @@ class CalendarFragment : Fragment() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        calendarView.dayBinder = object : DayBinder<DayViewContainer> {
-            override fun bind(container: DayViewContainer, day: CalendarDay) {
-                container.textView.text = day.date.dayOfMonth.toString()
-            }
-
-            override fun create(view: View) = DayViewContainer(view)
-
-        }
-
         val currentMonth = YearMonth.now()
         val firstMonth = currentMonth.minusMonths(10)
         val lastMonth = currentMonth.plusMonths(10)
@@ -64,24 +71,92 @@ class CalendarFragment : Fragment() {
         calendarView.setup(firstMonth, lastMonth, firstDayOfWeek)
         calendarView.scrollToMonth(currentMonth)
 
-//        val months = mutableListOf(
-//            "January",
-//            "February",
-//            "March",
-//            "April",
-//            "May",
-//            "June",
-//            "July",
-//            "August",
-//            "September",
-//            "October",
-//            "November",
-//            "December"
-//        )
+        if (savedInstanceState == null) {
+            calendarView.post {
+                // Show today's events initially.
+                selectDate(today)
+            }
+        }
+
+        class DayViewContainer(view: View) : ViewContainer(view) {
+            lateinit var day: CalendarDay
+            val textView = view.calendarDayText
+            val dotView = view.dotView
+
+            init {
+                view.setOnClickListener {
+                    if (day.owner == DayOwner.THIS_MONTH) {
+                        selectDate(day.date)
+                    }
+                }
+            }
+        }
+
+        calendarView.dayBinder = object : DayBinder<DayViewContainer> {
+            override fun bind(container: DayViewContainer, day: CalendarDay) {
+                container.day = day
+
+                val textView = container.textView
+                val dotView = container.dotView
+                textView.setTextColor(resources.getColor(R.color.mdtp_done_text_color_dark_disabled))
+                container.textView.text = day.date.dayOfMonth.toString()
+
+                if (day.owner == DayOwner.THIS_MONTH) {
+                    textView.visibility = View.VISIBLE
+                    when (day.date) {
+                        selectedDate -> {
+                            textView.setTextColor(Color.WHITE)
+                            textView.setBackgroundResource(R.drawable.calendar_selected_dot)
+                        }
+                        today -> {
+                            textView.setBackgroundResource(R.drawable.calendar_today_dot)
+                        }
+                        else -> {
+                            textView.setTextColor(resources.getColor(R.color.mdtp_done_text_color_dark_disabled))
+                            textView.background = null
+                        }
+                    }
+                } else {
+                    textView.visibility = View.INVISIBLE
+                    dotView.visibility = View.INVISIBLE
+                }
+            }
+
+            override fun create(view: View) = DayViewContainer(view)
+
+        }
+
+        calendarView.monthScrollListener = {
+
+            val actionbar = (activity as AppCompatActivity).supportActionBar
+
+            actionbar?.title = titleFormatter.format(it.yearMonth)
+
+            // Select the first day of the month when
+            // we scroll to a new month.
+            selectDate(it.yearMonth.atDay(1))
+        }
+
+        class MonthViewContainer(view: View) : ViewContainer(view) {
+            val legendLayout = view.legendLayout
+        }
 
     }
-}
 
-class DayViewContainer(view: View) : ViewContainer(view) {
-    val textView = view.calendarDayText
+    private fun selectDate(date: LocalDate) {
+        if (selectedDate != date) {
+            val oldDate = selectedDate
+            selectedDate = date
+            oldDate?.let { calendarView.notifyDateChanged(it) }
+            calendarView.notifyDateChanged(date)
+//            updateAdapterForDate(date)
+        }
+    }
+
+
+    override fun onDetach() {
+        super.onDetach()
+
+        (activity as AppCompatActivity).supportActionBar?.title = null
+    }
 }
